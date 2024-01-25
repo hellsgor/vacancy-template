bc75c004bf99a2806c4ae4989002d861a28f6311
Igor Storchevoy <hellsgor@gmail.com>
2024-01-25 13:08:40 +0400

    fix build

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import '@/assets/scss/index.scss'
-import { initSliders } from './js/sliders';
+
 
 // ---------------------------------
 
-window.addEventListener('DOMContentLoaded', () => {
-
-  // Utils
-  // ---------------------------------
-
-
-
-  // Modules
-  // ---------------------------------
-  initSliders()
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//
+//   // Utils
+//   // ---------------------------------
+//
+//
+//   // Modules
+//   // ---------------------------------
+// });
